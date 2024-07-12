386d7b328fe9280036e3d42dcdf3a59c7b06e550
Yogesh Jha <nityanandrekhajha@gmail.com>
2024-07-13 00:48:27 +0530

    Successfully created Header with Infinite ScrollX Animation

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const linkItems = document.querySelectorAll('.link-item');
 
+// Header Scroller Feature
+let currentScroll = 0;
+let isScrollingDown = true;
+
+let arrows = document.querySelectorAll(".arrow");
+
+let tween = gsap.to(".marquee_part",{
+    xPercent : -100,
+    repeat : -1,
+    duration: 10,
+    ease: 'linear'
+}).totalProgress(0.5);
+
+
+// NavBar ELements Feature
 linkItems.forEach((linkItem, index) => {
     linkItem.addEventListener("click", () => {
         // Remove active class from all link items
