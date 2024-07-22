793a3d141b76975f755c9389ac27c19d82252fab
Yogesh Jha <nityanandrekhajha@gmail.com>
2024-07-22 20:37:11 +0530

    Succesfully added custom cursor to the website

--- dist/script.js
+++ dist/script.js
@@ -27,3 +27,14 @@ linkItems.forEach((linkItem, index) => {
     });
 });
 
+// Custom Cursor
+document.addEventListener('mousemove', (e) => {
+    const cursor = document.getElementById('cursor');
+    const cursorBlur = document.getElementById('cursor-blur');
+    
+    cursor.style.top = `${e.clientY}px`;
+    cursor.style.left = `${e.clientX}px`;
+    
+    cursorBlur.style.top = `${e.clientY}px`;
+    cursorBlur.style.left = `${e.clientX}px`;
+});
