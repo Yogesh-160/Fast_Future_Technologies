83450fc20ef93d31a3c19b2afe4f44ad6f835b43
Yogesh Jha <nityanandrekhajha@gmail.com>
2024-07-29 01:15:30 +0530

    Added members page and updated various files

---
+++ dist/members.js
@@ -0,0 +1,7 @@
+// Custom Cursor
+document.addEventListener('mousemove', (e) => {
+    const cursor = document.getElementById('cursor');
+    
+    cursor.style.top = `${e.clientY}px`;
+    cursor.style.left = `${e.clientX}px`;
+});
