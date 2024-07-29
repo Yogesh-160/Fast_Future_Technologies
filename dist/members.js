5a59cce49e969d7cb717c7a132da3016ffc1e1f5
Yogesh Jha <nityanandrekhajha@gmail.com>
2024-07-29 14:06:16 +0530

    Updated members details

--- dist/members.js
+++ dist/members.js
@@ -19,53 +19,7 @@ document.addEventListener('mousemove', (e) => {
         ViewportY = window.innerHeight,
         listItems = Array.from(
           document.querySelectorAll(".list-item")
-        ),
-        // Paths
-        pathArr = [
-          {
-            id: 0,
-            path: createPathStr(
-              [74, 34.4, "L", 65.1, 26.8, "L", 68.1, 18.69, "L", 74.8, 24.9, "Z", "M", 74, 34.4, "L", 76.49, 43.2, "L", 78.4, 51.9, "L", 83.35, 55.2]
-            ),
-            description: "In fact this stars' sequence is an asterism within Ursa Major. that is now the third largest of the 88 modern constellations. Also The Big Dipper is used as a symbol of north as well as flag of Alaska.",
-          },
-          {
-            id: 1,
-            path: createPathStr(
-              [71.61, 83.66, "L", 74.47, 75.95, "L", 77.34, 78.68, "L", 73.95, 87.43, "Z", "M", 71.61, 83.6, "L", 66.66, 89.07, "L", 61.45, 89.07, "L", 56.77, 86.88]
-            ),
-            description: "The Ursa Minor has been one of the most important constellation. for navigation as it contains the Polaris being as North Star",
-          },
-          {
-            id: 2,
-            path: createPathStr(
-              [62.5, 12.56, "L", 46.875, 21.3, "L", 36.45, 27.86, "L", 28.125, 39.89, "M", 46.875, 21.30, "L", 47.65, 30, "L", 49.2, 36]
-            ),
-            description: "This constellation was named in honor of. Cassiopeia's daughter — Andromeda, in the Greek myth.",
-          },
-          {
-            id: 3,
-            path: createPathStr(
-              [19.27, 56.3, "L", 21.875, 50.8, "L", 20.3125, 46.45, "L", 16.66, 45.9, "L", 14, 45.4, "L", 13.28, 51.91 ,"L", 12.76, 61.75, "L", 9.65, 74.68, "L", 8.59, 77.6, "L", 6.8, 78.69, "M", 6.8, 78.69, "L", 4.7, 89.57, "M", 6.8, 78.69, "L", 3.39, 88.8, "M", 6.8, 78.69, "L", 2.3, 83.6, "M", 6.8, 78.69, "L", 2.35, 83.6, "M", 6.8, 78.69, "L", 2.3, 75.96, "M", 6.8, 78.69, "L", 2.1, 71.58]
-            ),
-            description: "Scorpio is one of the brightest constellations in the sky",
-          },
-          {
-            id: 4,
-            path: createPathStr(
-              [73.96, 45.36, "L", 77.6, 36.6, "L", 72.9, 33.3, "L", 72.1, 39.3, "Z", "M", 73.96, 45.36, "L", 73.96, 67.2, "L", 73.4, 78.1, "L", 68.75, 77, "L", 67.2, 68.3, "L", 66.4, 54.1, "L", 64.3, 44.8, "L", 62, 36.6, "L", 57.3, 36.1, "L", 45.3, 50.8, "L", 44, 54.1, "L", 40.1, 70.5, "L", 40.1, 86.3, "L", 45.3, 92.9]
-            ),
-            description: "One of the Drago's star called Kepler-10 has. Kepler-10b — smallest and closest to Earth exoplanet",
-          },
-          {
-            id: 5,
-            path: createPathStr(
-              [58.3, 7.1, "L", 60.2, 16.4, "L", 60.7, 20.21, "L", 60.2, 25.1, "L", 59.6, 26.8, "L", 57.3, 30, "L", 55.5, 31.7, "L", 42.7, 26.8, "L", 38.5, 29, "M", 57.3, 30, "L", 58.9, 35, "M", 58.9, 35, "L", 66.1, 33.3 , "L", 65.6, 29.5, "L", 66.4, 26.2, "L", 69.5, 20.2, "L", 70.6, 19.1, "L", 72.6, 18, "M", 58.9, 35, "L", 57.8, 41, "L", 59.1, 50.8, "L", 64.6, 59.6, "M", 59.1, 50.8, "L", 53.6, 55.2, "L", 52.6, 65, "M", 53.6, 55.2, "L", 47.9, 53.5, "L", 44.8, 57.4]
-            ),
-            description: "Sagittarius is one of the constellations of the zodiac. The center of the Milky Way lies in the westernmost part of Sagittarius",
-          }
-        ];
-   
+        )
     // Func 0
     function createPathStr(pArr) {
       let path = 'M';
